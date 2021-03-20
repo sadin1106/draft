edefdb72d3e8f74d9180170bf98271a1704db63c
sadin1106 <64366727+sadin1106@users.noreply.github.com>
2021-03-20 21:05:52 +0700

    first commit

---
+++ public/js/footer.js
@@ -0,0 +1,11 @@
+document.querySelector(".footer").innerHTML =
+  '<h6>Contact</h6>' +
+  '<div class="slogan">We are there for you &nbsp<i class="far fa-smile"></i></div>' +
+  '<div class="contact">' +
+  '<div class="contact-icon">' +    
+    '<span><i class="fas fa-phone"></i></span>'+
+    '<span><i class="fas fa-envelope-square"></i></i></span>'+
+    '<span><i class="fab fa-facebook"></i></i></i></span>'+
+  '</div>'+
+  '<div class="message"><i class="fas fa-comments"></i></div>'+
+'</div>';
